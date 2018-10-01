491b4734f853a2835c300f862f16f2c0b0937c1b
shubh-ng <40854895+shubh-ng@users.noreply.github.com>
2018-10-01 14:30:21 +0530

    Update timeConversion.java

--- 500-program/timeConversion.java
+++ 500-program/timeConversion.java
@@ -1,23 +1,25 @@
-
-    static String timeConversion(String s) {
+ static String timeConversion(String s) {
+        /*
+         * Write your code here.
+         */
         String TwentyFourTime=null;
-        int hours=0;
+        int hours=0,min=0;
         String[] times = s.split(":");  //taking : separated values
+        min = Integer.parseInt(times[1]); //taking minute as integer input
         hours = Integer.parseInt(times[0]); //taking hour as integer input
         char[] secs = times[2].toCharArray(); //taking seconds
         char c = times[2].toCharArray()[2]; //taking P or A for matching PM or AM
-        if(hours==12 && c=='A'){    //if 12 morning
-            TwentyFourTime = "00:"+times[1]+":"+times[2]+":AM";
+        if(hours==12 && min>=0 && c=='A'){    //if 12 morning
+            TwentyFourTime = "00:"+times[1]+":"+secs[0]+secs[1];
             return TwentyFourTime;
         }
-        if(hours==12 && c=='P'){    //if 12 noon
-            TwentyFourTime = s;
-            return TwentyFourTime;
+        if(hours==12 && min>=0 && c=='P'){    //if 12 noon
+            return times[0]+":"+times[1]+":"+secs[0]+secs[1];
         }
         if(c=='P'){ //if after noon
             hours += 12;
             TwentyFourTime = hours+":"+times[1]+":"+secs[0]+secs[1];
             return TwentyFourTime;
         }
-        return s;
+        return times[0]+":"+times[1]+":"+secs[0]+secs[1];
     }
